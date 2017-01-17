5c75fcd6eb82a9d0fff49088935386a0893f12d9
Jonathan Larson <jonathanl@eleveninc.com>
2017-01-17 12:52:25 -0800

    added rotor anim to about page

--- assets/js/controllers/aboutUsView.js
+++ assets/js/controllers/aboutUsView.js
@@ -9,11 +9,50 @@ angular.module('Earthly.controllers')
 
         var tl1 = new TimelineMax({delay: 0});
 
+        tl1.to('.turbine-main', .5, { opacity: 1, ease: Power0.easeNone  }, 'firstFrame');
+        tl1.to('.rotor-main', 1, { opacity: 1, ease: Power0.easeNone  }, 'firstFrame');
+        tl1.to('.rotor-left', 1, { opacity: 1, ease: Power0.easeNone  }, 'firstFrame');
+        tl1.to('.rotor-right', 1, { opacity: 1, ease: Power0.easeNone  }, 'firstFrame');
+
         tl1.to('.below-hero', .5, { opacity: 1, ease: Power0.easeNone  }, '-=0.4');
+
+        tl1.to('.rotor-main', 20, { rotation: 360, ease: Power0.easeNone,repeat: -1  }, 'firstFrame');
+        tl1.to('.rotor-left', 19, { rotation: 360, ease: Power0.easeNone,repeat: -1  }, 'firstFrame');
+        tl1.to('.rotor-right', 22, { rotation: 360, ease: Power0.easeNone,repeat: -1  }, 'firstFrame');
+
+
 
     };
 
+    var tl2 = new TimelineMax({repeat:-1});
+    var tl3 = new TimelineMax({repeat:-1,delay: .5});
+    var tl4 = new TimelineMax({repeat:-1,delay: .8});
+
+    function cloudSmall() {
+      tl2.to('.cloud-small', 1, {opacity: '.3', left:"10", ease:Power0.easeNone});
+      tl2.to('.cloud-small', 65, {left:"698", ease:Power0.easeNone});
+      tl2.to('.cloud-small', 1, {opacity: '0', left:"700", ease:Power0.easeNone});
+      tl2.to('.cloud-small', .1, {left:"0", ease:Power0.easeNone});
+    }
+
+    function cloudMedium() {
+      tl3.to('.cloud-medium', 1, {opacity: '.4', left:"12", ease:Power0.easeNone});
+      tl3.to('.cloud-medium', 72, {left:"698", ease:Power0.easeNone});
+      tl3.to('.cloud-medium', 1, {opacity: '0', left:"700", ease:Power0.easeNone});
+      tl3.to('.cloud-medium', .1, {left:"0", ease:Power0.easeNone});
+    }
+
+    function cloudLarge() {
+      tl4.to('.cloud-large', 1, {opacity: '.2', left:"15", ease:Power0.easeNone});
+      tl4.to('.cloud-large', 48, {left:"698", ease:Power0.easeNone});
+      tl4.to('.cloud-large', 1, {opacity: '0', left:"700", ease:Power0.easeNone});
+      tl4.to('.cloud-large', .1, {left:"0", ease:Power0.easeNone});
+    }
+
     startAnimations();
+    cloudSmall();
+    cloudMedium();
+    cloudLarge();
 
     //  window.sr = ScrollReveal().reveal('.container, .site-footer');
     // input comma-adder
