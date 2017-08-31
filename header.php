24d0a875cb9e0c4c4e5d4fca4c34fcb103ff79e7
Jonathan Larson <jonathanl@eleveninc.com>
2017-08-31 15:36:45 -0700

    meta tag update

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
   <meta property="og:type" content="website"/>
   <!-- <meta property="og:title" content="Earthly - Sustainable Investment Management and Advice"/>
   <meta property="og:image" content="/wp-content/themes/earthly-2016/build/images/global/earthly.png"/> -->
-  <meta property="og:description" content="Earthly offers investment advisory and management services based on the principles of passive investment management, sustainable investing and socially responsible investing in order to provide a low-cost and sustainable investment platform for individual investors and institutions."/>
+  <!-- <meta property="og:description" content="Earthly offers investment advisory and management services based on the principles of passive investment management, sustainable investing and socially responsible investing in order to provide a low-cost and sustainable investment platform for individual investors and institutions."/> -->
   <meta property="og:url" content="https://www.beearthly.com/#!/" />
   <link rel="icon" type="image/png" href="wp-content/themes/earthly-2016/build/images/icons/favicon-32x32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="wp-content/themes/earthly-2016/build/images/icons/favicon-16x16.png" sizes="16x16">
